da793d278a34b4f085b840c5184270c518128f89
ErickLemus7 <lh202201@upes.edu.sv>
2025-06-25 18:52:09 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Uso de Operadores Condicinales
+        if (1<0)
+        {
+            // Código si la condición es verdadera
+        }
+        else
+        {
+            // Código si la condición es falsa
+        }
+    }
+
+    public void Ejemplo()
+    {
+        int edad = 18;
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad.");
+        }
+        else
+        {
+            Console.WriteLine("Eres menor de edad.");
+        }
+    }
+
+    //Ejemplo Usando if-else-if-else
+    public void EjemploIfElse()
+    {
+        int nota = 75;
+        if (nota >= 90)
+        {
+            Console.WriteLine("Excelente");
+        }
+        else if (nota >= 70)
+        {
+            Console.WriteLine("Bueno");
+        }
+        else
+        {
+            Console.WriteLine("Necesitas mejorar");
+        }
+    }
+}
