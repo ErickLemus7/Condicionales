7cc0583224aab747ca6e9a8ff892d7a2908fbdaa
ErickLemus7 <lh202201@upes.edu.sv>
2025-06-25 20:42:10 -0600

    #Metodos de Comparion y Logicos

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     public static void Main(string[] args)
     {
+        int x = 10;
         //Uso de Operadores Condicinales
-        if (1<0)
+        if (x==10)
         {
             // Código si la condición es verdadera
         }
@@ -13,9 +14,14 @@ class Program
         {
             // Código si la condición es falsa
         }
+
+        //Ejecutador
+        
+
     }
 
-    public void Ejemplo()
+    //Ejemplo If-Else
+    public static void Ejemplo()
     {
         int edad = 18;
         if (edad >= 18)
@@ -29,20 +35,105 @@ class Program
     }
 
     //Ejemplo Usando if-else-if-else
-    public void EjemploIfElse()
+    public static void EjemploIfElse()
     {
         int nota = 75;
         if (nota >= 90)
         {
             Console.WriteLine("Excelente");
         }
-        else if (nota >= 70)
+        else if (nota >= 80)
+        {
+            Console.WriteLine("Muy Bueno");
+        }
+        else if (nota >=70)
         {
             Console.WriteLine("Bueno");
         }
         else
         {
-            Console.WriteLine("Necesitas mejorar");
+            Console.WriteLine("Necesita Mejorar");
+        }
+        Console.WriteLine("Nota ha sido evaluada");
+    }
+
+    public static void EjemploIgual()
+    {
+        int edad = 18;
+        bool tieneLicencia = true;
+
+        if (edad >= 18)
+        {
+            if (tieneLicencia == true)
+            {
+                Console.WriteLine("Puedes Manejar");
+            }
+            else
+            {
+                Console.WriteLine("No Puedes Manejar");
+            }
+        }
+        else 
+        {
+            Console.WriteLine("No hay edad Suficiente");
+        }
+    }
+
+    public static void EjemploAnd()
+    {
+        int edad = 18;
+        bool tieneLicencia = true;
+
+        if (edad >= 18 && tieneLicencia==true)
+        {
+            Console.WriteLine("Puedes Conducir");
+        }
+        else
+        {
+            Console.WriteLine("No Puedes conducir");
+        }
+    }
+
+    public static void EjemploOr()
+    {
+        string dia = "Sabado";
+        //Detectar si es fin de semana
+        if (dia == "Sabado" || dia == "Domingo")
+        {
+            Console.WriteLine("Es fin de semana");
+        }
+        else
+        {
+            Console.WriteLine("Es dia de semana");
+        }
+    }
+
+    public static void EjemploNegacion()
+    {
+        int edad = 18;
+        bool tieneLicencia = true;
+
+        if (!(edad >= 18 && tieneLicencia == true))
+        {
+            Console.WriteLine("No Puedes conducir");
+        }
+        else
+        {
+            Console.WriteLine("Puedes Conducir");
+        }
+    }
+
+    public static void EjemploDiferente()
+    {
+        int dado = 5;
+
+        if (dado != 5)
+        {
+            Console.WriteLine("Procede a premio");
+        }
+        else
+        {
+            Console.WriteLine("No hay premio");
         }
     }
 }
